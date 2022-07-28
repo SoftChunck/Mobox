7e1cfa9cb233be1fa5b633f3af2394b682ff0dc5
Usama <softchunck@gmail.com>
2022-07-28 06:25:10 -0700

    Mobox Dag Working

--- src/Containers/MomoboxDag/MomoboxDag.jsx
+++ src/Containers/MomoboxDag/MomoboxDag.jsx
@@ -1,9 +1,78 @@
 import React from "react";
 import './MomoboxDag.css'
 const MomoboxDag = () => {
+    React.useEffect(()=>{
+        let menu_item_btn = document.querySelectorAll('.menu-item-btn');
+        let menu_item_anim = document.querySelectorAll('.menu-item-anim');
+        let img_dag = document.querySelector('.img-dag');
+        let text_dag = document.querySelectorAll('.text-item-dag');
+        let img_dag_clone = document.querySelector('.img-dag-clone');
+        for(let x=0;x<3;x++)
+        {
+            menu_item_btn[x].addEventListener('click',()=>{
+                for(let y=0;y<3;y++)
+                {   
+                    text_dag[y].classList.add('d-none');
+                    img_dag_clone.classList.remove(`paint${y}-clone`);
+                    img_dag.classList.remove(`paint${y}`);                
+                    menu_item_anim[y].setAttribute('style',`border-bottom-color:transparent;width:0px;`);                    
+                } 
+                text_dag[x].classList.remove('d-none');
+                menu_item_anim[x].setAttribute('style',`border-bottom-color:orange;width:100%;`);
+                img_dag_clone.classList.add(`paint${x}-clone`);
+                img_dag.classList.add(`paint${x}`);                           
+                img_dag_clone.classList.remove('img-dag-animate');
+                setTimeout(() => {
+                    img_dag_clone.classList.add('img-dag-animate');    
+                }, 100);      
+            })
+        }
+    })
     return ( 
-        <div>
-
+        <div className="Moboxdag row justify-content-center mt-5 pt-5"> 
+            <div className="slider-dag col-lg-1">
+                <img src="assets/images/vision-slider.png" />
+            </div>
+            <div className="col-lg-10 row justify-content-between align-items-center mt-5">
+                <div className="text-dag-container col-lg-4 mt-3 row">
+                    <div className="menu-dag mt-5">
+                        <div className="menu-links col-lg-10 pb-5">
+                            <div className="menu-item col-lg-12 py-2">
+                                <a className="menu-item-btn font_PB fs-1">NFT Creator</a>
+                                <div className="menu-item-anim"></div>
+                            </div>
+                            <div className="menu-item col-lg-12 py-2">
+                                <a className="menu-item-btn font_PB fs-1">Game Creator</a>
+                                <div className="menu-item-anim"></div>
+                            </div>
+                            <div className="menu-item col-lg-12 py-2">
+                                <a className="menu-item-btn font_PB fs-1">MOBOX Dag</a>
+                                <div className="menu-item-anim"></div>
+                            </div> 
+                        </div>                 
+                        <div className="text-dag">
+                            <div className="text-item-dag">
+                                <h1 className="display-1 font_PB">NFT Creator</h1>
+                                <p className="fs-5">Get rewarded for your fabulous creations. Users can design and mint their own NFTs to be used in the MOMOverse. Original creators receive lifetime trading fees for their NFT creations.</p>
+                            </div>
+                            <div className="text-item-dag d-none">
+                                <h1 className="display-1 font_PB">Game Creator</h1>
+                                <p className="fs-5">Easily launch your blockchain game in the MOMOverse. With simple to use APIs, game developers can take advantage of everything the MOMOverse has to offer including free to play and play to earn ecosystem, NFT Marketplaces, MOMO NFTs and more.Got a great game but don’t know where to start? Check out the MOBOX METAfund.</p>
+                            </div>
+                            <div className="text-item-dag d-none">
+                                <h1 className="display-1 font_PB">MOBOX DAG</h1>
+                                <p className="fs-5">Create and organize your own Decentralized Autonomous Guild. Using trustless Smart Contracts, start your own MOMO community, share NFT resources, grow your treasury, and have fun and earn together.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="img-dag col-lg-6 paint0">
+                    <img src="assets/images/paint0.png" className="col-12 img-dag-v"/>                    
+                    <div className="img-dag-clone paint0-clone img-dag-animate">
+                        <img src="assets/images/paint0.png" className="col-12 img-dag-v"/>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
--- src/Containers/Momoverse/Momoverse.jsx
+++ src/Containers/Momoverse/Momoverse.jsx
@@ -1,12 +1,92 @@
+import { MoveToInboxRounded } from "@material-ui/icons";
 import React from "react";
 import './Momoverse.css'
 const Momoverse = () => {
+    React.useEffect(()=>{
+        let momo_div = document.querySelector('.momo-text-div');
+        let momo_slider = document.querySelector('.momo-slider');
+        momo_slider.setAttribute('style',`height:${momo_div.clientHeight}px;`);
+        let momoverse_a = document.querySelectorAll('.momoverse-a');
+        let active_div = document.querySelectorAll('.active-div');
+        let verse_img = document.querySelector('.img-verse');
+
+        momoverse_a[0].addEventListener('click',()=>{
+            for(let y=0;y<3;y++)
+            {                
+                active_div[y].setAttribute('style',`border-bottom-color:transparent;width:0px;`);                    
+            }
+            active_div[0].setAttribute('style',`border-bottom-color:#1949c6;width:${momoverse_a[0].clientWidth}px`);
+            verse_img.setAttribute('style','opacity:0;transform:translateX(20px);');
+            setTimeout(() => {                
+                verse_img.setAttribute('src','assets/images/verse1.png');
+                verse_img.setAttribute('style','opacity:1;transform:translateX(0px);');
+            }, 450);
+        })
+        momoverse_a[1].addEventListener('click',()=>{
+            for(let y=0;y<3;y++)
+            {
+                active_div[y].setAttribute('style',`border-bottom-color:transparent;width:0px;`);                    
+            }
+            active_div[1].setAttribute('style',`border-bottom-color:#1949c6;width:${momoverse_a[1].clientWidth}px`);
+            verse_img.setAttribute('style','opacity:0;transform:translateX(20px);');
+            setTimeout(() => {                
+                verse_img.setAttribute('src','assets/images/verse2.png');
+                verse_img.setAttribute('style','opacity:1;transform:translateX(0px);');
+            }, 450);
+        })
+        momoverse_a[2].addEventListener('click',()=>{
+            for(let y=0;y<3;y++)
+            {
+                active_div[y].setAttribute('style',`border-bottom-color:transparent;width:0px;`);                    
+            }
+            active_div[2].setAttribute('style',`border-bottom-color:#1949c6;width:${momoverse_a[2].clientWidth}px`);
+            verse_img.setAttribute('style','opacity:0;transform:translateX(20px);');
+            setTimeout(() => {           
+                verse_img.setAttribute('src','assets/images/verse3.png');   
+                verse_img.setAttribute('style','opacity:1;transform:translateX(0px);');
+            }, 450);
+        })
+    }
+    )
     return ( 
-        <div className="Momoverse row justify-content-center">
-            <div className="mt-5 pt-5 col-10 justify-content-center row">
-                <h1 className="col-12 font_PB display-1 text-center mt-5 p-5 text-white">MOBOverse</h1>
+        <div className="Momoverse row justify-content-center m-0">
+            <div className="mt-5 pt-5 col-md-10 justify-content-center row m-0">
+                <h1 className="col-12 font_PB display-1 text-center mt-5 pt-5 text-white ">MOBOverse</h1>
                 <p className="col-md-8 col-10 text-white text-center">The MOMOverse is a cross platform metaverse that can be enjoyed on all devices no matter where you are. Whether it is creating content or enjoying others’ creations, the MOMOverse rewards players, developers, and collectors for their engagement and enjoyment.</p>
             </div>
+            <div className="row justify-content-around m-0">
+                <div className="row col-md-11 col-lg-10 align-items-center justify-content-between  momo-text-div m-0">
+                        <div className="col-md-12 col-lg-7 justify-content-center order-3 order-lg-1">
+                            <div className="row m-0">
+                                <img src="assets/images/verse1.png" className="img-verse" />
+                            </div>
+                            <p className="row text-white mb-5 pt-2">MOBOX Blind Boxes are real life blind boxes that contain unique MOMO figurines with a QR code on each one. Players can scan the QR code and the same unique MOMO is immediately minted on the Binance Smart Chain and used across the MOMOverse.</p>  
+                        </div>
+                        <div className="row col-md-12 col-lg-4 col-xl-3 m-0 justify-content-end align-items-start order-2 order-lg-2 text-div-verse">
+                            <div className="col-md-4 col-lg-11 py-2 ">
+                                <a className="font_PB momoverse-a fs-3 row m-0">MOMO Avatar</a>
+                                <div className="active-div"></div>
+                            </div>
+                            <div className="col-md-4 col-lg-11 py-2">
+                                <a className="font_PB momoverse-a fs-3 row m-0">MOMO NFTs</a>
+                                <div className="active-div"></div>
+                            </div>
+                            <div className="col-md-4 col-lg-11 py-2">
+                                <a className="font_PB momoverse-a fs-3 row m-0">Blind Box</a>
+                                <div className="active-div "></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1 momo-slider d-none d-lg-flex">
+                        <img src="assets/images/moboverse-slider.png" className="img-slide-verse"/>
+                    </div>
+            </div>
+            <div className="dis row">
+            <div className="row justify-content-end m-0 decorate-div-verse">
+                <img src="assets/images/decorate.png" className="decorate-img-verse col-3" />
+           </div>
+            </div>
+           
         </div>
      );
 }
